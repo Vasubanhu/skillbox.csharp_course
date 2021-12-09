1678bab6482e824169b4d775165ed76818a39ddc
Leonid Bedrikov <shangrila1721@gmail.com>
2021-12-09 18:49:43 +0500

    initial commit

---
+++ exercise_1/exercise_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region №1
+
+            //WriteLine("HelloWorld!!!");
+
+            //ReadKey();
+
+            #endregion
+            #region №2
+
+            Write("Hello ");
+            Write("World ");
+            Write("!!!");
+
+            ReadLine();
+
+            #endregion
+        }
+    }
+}
