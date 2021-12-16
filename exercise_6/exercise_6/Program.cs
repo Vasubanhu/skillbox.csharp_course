6d9187f84a70919ac222fbd63011da39bd9eee26
Leonid Bedrikov <shangrila1721@gmail.com>
2021-12-16 13:24:31 +0500

    initial commit

---
+++ exercise_6/exercise_6/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace exercise_6
+{
+    internal class Program
+    {
+        private static readonly string _fileName = $@"{Directory.GetCurrentDirectory()}\employees.txt";
+        private static readonly char _separator = '#';
+        private static string _dateTimePattern = "dd.MM.yy HH:mm";
+        private static string _dashPattern = "---------------------------------------------------------------------------------------------------------";
+        private static string _headerPattern = @"|ID                             |Дата и время  |Ф.И.О.  |Возраст  |Рост  |Дата рождения  |Место рождения|";
+
+        private static void Main()
+        {
+            WriteLine("Введите номер операции и нажмите Enter.\n" + "\n" +
+                      "* Вывести данные о сотруднике на экран - 1;\n" +
+                      "* Заполнить данные о сотруднике и добавить новую запись в конец файла - 2.");
+
+            string action = ReadLine();
+
+            switch (action)
+            {
+                case "1":
+                    Clear();
+                    WriteLine($"Сотрудники:\n{_dashPattern}");
+                    WriteLine($"{_headerPattern}");
+                    WriteLine($"{ _dashPattern}");
+                    ReadData(_fileName);
+                    break;
+                case "2":
+                    Clear();
+                    WriteData(InputData());
+                    break;
+                default:
+                    WriteLine("Некорректный номер операции.");
+                    break;
+            }
+
+            WriteLine("\nДля выхода нажмите Enter.");
+            ReadKey();
+        }
+
+        #region Methods
+        private static string Trim(string text)
+        {
+            string[] words;
+            words = text.Split('#');
+            return string.Join(" ", words);
+        }
+
+        private static string GenerateID() => string.Format($"{Guid.NewGuid():N}");
+
+        private static void ReadData(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                using (StreamReader streamReader = new StreamReader(fileName))
+                {
+                    while (streamReader.Peek() >= 0)
+                    {
+                        WriteLine(Trim(streamReader.ReadLine()));
+                    }
+                }
+            }
+
+            else
+            {
+                WriteLine("Файл не найден.");
+            }
+        }
+
+        private static string InputData()
+        {
+            string record = String.Empty;
+            string timeStamp;
+            string id = GenerateID();
+
+            record += id + _separator;
+            Write($"Введите данные сотрудника.\n-------------------------\nID: {id}\n");
+
+            timeStamp = DateTime.Now.ToString(_dateTimePattern);
+            Write($"Дата и время добавления записи: {timeStamp}\n");
+            record += timeStamp + _separator;
+
+            Write("Ф.И.О.: ");
+            record += ReadLine() + _separator;
+
+            Write("Возраст: ");
+            record += ReadLine() + _separator;
+
+            Write("Рост: ");
+            record += ReadLine() + _separator;
+
+            Write("Дата рождения: ");
+            record += ReadLine() + _separator;
+
+            Write("Место рождения: ");
+            record += ReadLine();
+
+            return record;
+        }
+
+        private static void WriteData(string data)
+        {
+            if (File.Exists(_fileName))
+            {
+                using (StreamWriter streamWriter = File.AppendText(_fileName))
+                {
+                    streamWriter.WriteLine(data);
+                }
+            }
+
+            else
+            {
+                using (StreamWriter streamWriter = File.CreateText(_fileName))
+                {
+                    streamWriter.WriteLine(data);
+                }
+            }
+        }
+        #endregion
+    }
+}
--- exercise_6/exercise_6/Properties/AssemblyInfo.cs
+++ exercise_6/exercise_6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("exercise_5_1")]
+[assembly: AssemblyTitle("exercise_6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("exercise_5_1")]
+[assembly: AssemblyProduct("exercise_6")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b1d7d3f-b2af-4609-95f4-513ff9bf3da0")]
+[assembly: Guid("f1891567-58e9-4fa2-bb04-fd135c107851")]
 
 // Version information for an assembly consists of the following four values:
 //
