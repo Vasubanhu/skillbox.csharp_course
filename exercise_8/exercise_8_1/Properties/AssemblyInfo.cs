cdd9449b3781ada1099dcfaef34b82317f314da9
Leonid Bedrikov <shangrila1721@gmail.com>
2021-12-20 22:19:46 +0500

    Add task_1

--- exercise_8/exercise_8_1/Properties/AssemblyInfo.cs
+++ exercise_8/exercise_8_1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("exercise_7")]
+[assembly: AssemblyTitle("exercise_8_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("exercise_7")]
+[assembly: AssemblyProduct("exercise_8_1")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3adf9154-d646-4490-bea1-10f0c11f2335")]
+[assembly: Guid("59e3d882-dcb9-498a-ba08-1c575571808e")]
 
 // Version information for an assembly consists of the following four values:
 //
