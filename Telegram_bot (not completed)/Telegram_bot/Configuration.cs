c95c4a21c0560b2852df99214c4c1c3c132a6dd8
Leonid Bedrikov <shangrila1721@gmail.com>
2022-03-17 16:23:50 +0500

    test commit

---
+++ Telegram_bot (not completed)/Telegram_bot/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Telegram_bot
+{
+    internal static class Configuration
+    {
+        public static readonly string Token = "5031242663:AAFCwwmC0ih-9Xhzh70bzgOitny5o1DkxMk";
+    }
+}
---
+++ Telegram_bot (not completed)/Telegram_bot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace Telegram_bot
+{
+    internal class Program
+    {
+        private static TelegramBotClient _botClient;
+
+        private static async Task Main(string[] args)
+        {
+            _botClient = new TelegramBotClient(Configuration.Token);
+
+            // Test connection
+            User user = await _botClient.GetMeAsync();
+            Title = user.Username ?? "My awesome Bot";
+
+            using CancellationTokenSource cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            ReceiverOptions receiverOptions = new ReceiverOptions { AllowedUpdates = { } }; // receive all update types
+            _botClient.StartReceiving(Handlers.HandleUpdateAsync, 
+                                      Handlers.HandleErrorAsync, 
+                                      receiverOptions, 
+                                      cancellationToken: cts.Token);
+
+            WriteLine($"Start listening for @{user.Username}");
+            ReadKey();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+    }
+}
