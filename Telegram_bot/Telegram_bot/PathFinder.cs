fc6865c64a4a80ed325ae3cd2ebb925d3efba477
Leonid Bedrikov <shangrila1721@gmail.com>
2022-05-13 11:06:57 +0500

    Small improvements.

---
+++ Telegram_bot/Telegram_bot/PathFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Telegram_bot
+{
+    internal class PathFinder
+    {
+        private static readonly string Template = @$"Users\{Environment.UserName}\Downloads\Telegram Desktop";
+
+        internal static string SetPath() => DriveInfo.GetDrives()
+            .Where(d => d.IsReady)
+            .Select(d => Path.Combine(d.Name, Template))
+            .Select(path => Directory.Exists(path) ? path : "Path not exist.").FirstOrDefault();
+    }
+}
