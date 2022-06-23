d9658d502138322a033a0806a03094c016ded4b8
Leonid Bedrikov <shangrila1721@gmail.com>
2022-06-23 17:23:37 +0500

    Add some classes.

---
+++ exercise_11/exercise_11/Consultant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_11
+{
+    internal class Consultant
+    {
+        internal virtual List<Customer> GetInfo(List<Customer> list)
+        {
+            foreach (Customer customer in list)
+            {
+                customer.PassportDetails = customer.PassportDetails != null ? "** *******" : "n/a";
+            }
+
+            return list;
+        }
+
+        //internal virtual Customer Edit(Customer customer)
+        //{
+
+        //}
+    }
+}
---
+++ exercise_11/exercise_11/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_11
+{
+    internal class Customer
+    {
+        private string _firstName;
+        private string _middleName;
+        private string _lastName;
+        private string _phoneNumber;
+        private string? _passportDetails;
+
+        public Customer(string firstName, string middleName, string lastName, string phoneNumber, string passportDetails)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            PassportDetails = passportDetails;
+        }
+
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string MiddleName { get => _middleName; set => _middleName = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string PassportDetails { get => _passportDetails; set => _passportDetails = value; }
+    }
+}
